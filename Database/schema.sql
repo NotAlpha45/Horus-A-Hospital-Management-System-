4a1b57e55cf39c8b7bf2e2335546bdab92fd91e8
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2022-04-16 22:57:16 +0600

    Schema for diagnostics, appointments and doctors

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,14 +1,36 @@
 CREATE TABLE Patients (
     patient_id INTEGER PRIMARY KEY NOT NULL,
-    patient_name VARCHAR(60) NOT NULL,
+    patient_name VARCHAR(255) NOT NULL,
     patient_gender VARCHAR2(7) NOT NULL,
     patient_blood_group VARCHAR2(5) NOT NULL,
     patient_dob DATE NOT NULL NOT NULL,
     patient_phone_number NUMBER(11,0) NOT NULL,
-    patient_address VARCHAR2(60)
-)
+    patient_address VARCHAR2(255)
+);
 
 CREATE TABLE Doctors (
     doctor_id INTEGER PRIMARY KEY NOT NULL,
+    doctor_name VARCHAR(255) NOT NULL,
+    doctor_designation VARCHAR2(255) NOT NULL,
+    license_year INTEGER NOT NULL,
+    doctor_phone_number NUMBER(11,0) NOT NULL,
+    doctor_address VARCHAR2(255) NOT NULL
+);
 
+CREATE TABLE Appointments (
+    app_id INTEGER PRIMARY KEY NOT NULL,
+    patient_id INTEGER,
+    doctor_id INTEGER,
+    app_date DATE
 )
+
+CREATE TABLE Diagnostics (
+    diag_id INTEGER PRIMARY KEY NOT NULL,
+    patient_id INTEGER NOT NULL,
+    doctor_id INTEGER NOT NULL,
+    diag_details TEXT,
+    diag_remarks TEXT,
+    diag_date DATE,
+    FOREIGN KEY (patient_id) REFERENCES Patients (Patient_id) ON DELETE CASCADE,
+    FOREIGN KEY (doctor_id) REFERENCES Doctors (doctor_id)
+);
