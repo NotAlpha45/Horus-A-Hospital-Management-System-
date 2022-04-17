4023dd71a3bb9a101c2664203412e620bda3ffed
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2022-04-17 17:34:37 +0600

    Trigger for appointment ID

--- Database/function_and_trigger.sql
+++ Database/function_and_trigger.sql
@@ -1,3 +1,4 @@
+-- Function and trigger for patient id.
 CREATE OR REPLACE FUNCTION generate_patient_id (phone_no NUMBER)
 RETURN INTEGER
 IS
@@ -22,14 +23,16 @@ BEGIN
     RETURN next_seq;
 
 END;
+/
 
 CREATE OR REPLACE TRIGGER generate_patient_id BEFORE INSERT 
 ON Patients FOR EACH ROW 
 BEGIN
     :NEW.patient_id := generate_patient_id(:NEW.patient_phone_number);
 END;
+/
 
-
+-- Function and trigger for doctor id.
 CREATE OR REPLACE FUNCTION generate_doctor_id (license_number NUMBER)
 RETURN INTEGER
 IS
@@ -54,9 +57,72 @@ BEGIN
     RETURN next_seq;
 
 END;
+/
 
-CREATE OR REPLACE TRIGGER generate_doctor_id BEFORE INSERT 
-ON Doctors FOR EACH ROW 
+CREATE OR REPLACE TRIGGER generate_doctor_id 
+BEFORE INSERT ON Doctors 
+FOR EACH ROW 
 BEGIN
     :NEW.doctor_id := generate_doctor_id(:NEW.doctor_license_no);
 END;
+/
+
+-- Function and trigger for diagnosis id.
+CREATE OR REPLACE FUNCTION generate_diag_id(diagnostics_date DATE, diag_patient_id INTEGER)
+RETURN INTEGER
+IS 
+
+    diag_day INTEGER := EXTRACT (DAY FROM diagnostics_date);
+    diag_month INTEGER := EXTRACT (MONTH FROM diagnostics_date);
+    diag_year INTEGER := EXTRACT (YEAR FROM diagnostics_date);
+
+
+BEGIN 
+
+    RETURN TO_NUMBER(
+    TO_CHAR(diag_day) || 
+    TO_CHAR(diag_month) || 
+    TO_CHAR (diag_year) || 
+    TO_CHAR(diag_patient_id)
+    );
+
+END;
+/
+
+CREATE OR REPLACE TRIGGER generate_diag_id 
+BEFORE INSERT ON Diagnostics
+FOR EACH ROW
+BEGIN
+    :NEW.diag_id := generate_diag_id(:NEW.diag_date, :NEW.patient_id);
+END; 
+/
+
+-- Function and trigger for appintment ID.
+CREATE OR REPLACE FUNCTION generate_app_id(app_date DATE, app_doctor_id INTEGER)
+RETURN INTEGER
+IS 
+
+    app_day INTEGER := EXTRACT (DAY FROM app_date);
+    app_month INTEGER := EXTRACT (MONTH FROM app_date);
+    app_year INTEGER := EXTRACT (YEAR FROM app_date);
+
+
+BEGIN 
+
+    RETURN TO_NUMBER(
+    TO_CHAR(app_day) || 
+    TO_CHAR(app_month) || 
+    TO_CHAR (app_year) || 
+    TO_CHAR(app_doctor_id)
+    );
+
+END;
+/
+
+CREATE OR REPLACE TRIGGER generate_app_id 
+BEFORE INSERT ON Appointments
+FOR EACH ROW
+BEGIN
+    :NEW.app_id := generate_app_id(:NEW.app_date, :NEW.doctor_id);
+END; 
+/
--- Database/sample_queries.sql
+++ Database/sample_queries.sql
@@ -1,5 +1,19 @@
 INSERT INTO Patients(patient_name, patient_gender, patient_blood_group, patient_dob, patient_phone_number, patient_address) VALUES
 ('a', 'male', 'a+', TO_DATE('12-12-2021', 'DD-MM-YYYY'), 01712181229, 'c');
+-- Patient ID generated : 17121812291
 
-INSERT INTO Patients(doctor_name, doctor_designation, doctor_license_no, doctor_phone_number, doctor_address) VALUES
-('a', 'surgeon', 42069, 01712181229, 'cd');
+INSERT INTO Doctors(doctor_name, doctor_designation, doctor_license_no, doctor_phone_number, doctor_address) VALUES
+('a', 'surgeon', 42069, 01712181229, 'csa');
+-- Doctor ID generated : 420691
+
+DECLARE
+    dt DATE := SYSDATE;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(dt);
+END;
+
+INSERT INTO Diagnostics (patient_id, doctor_id, diag_details, diag_remarks, diag_date) 
+VALUES (17121812291, 420691, 'uwieryuiqweu', 'zxcgxzbcvcx', TO_DATE('15-APR-2022', 'DD-MON-YYYY'));
+
+INSERT INTO Appointments (patient_id, doctor_id, app_date)
+VALUES (17121812291, 420691, TO_DATE('15-APR-2022', 'DD-MON-YYYY'));
